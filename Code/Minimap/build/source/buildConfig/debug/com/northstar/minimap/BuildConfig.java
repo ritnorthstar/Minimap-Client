dc0651a34005600356d8dab8da9bf0960ede8927
Benjin Dubishar <benjin.dubishar@gmail.com>
2013-10-03 14:25:13 -0400

    Initial check-in of Android code

---
+++ Code/Minimap/build/source/buildConfig/debug/com/northstar/minimap/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.northstar.minimap;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+
+}
