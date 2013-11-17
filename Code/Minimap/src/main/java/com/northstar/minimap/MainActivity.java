a6aa0a8c626824732b8c06ab15ac0a5f8991ec89
Chris Carey <cpc2273@rit.edu>
2013-11-17 14:44:24 -0500

    Left drawer.

--- Code/Minimap/src/main/java/com/northstar/minimap/MainActivity.java
+++ Code/Minimap/src/main/java/com/northstar/minimap/MainActivity.java
@@ -1,54 +1,101 @@
 package com.northstar.minimap;
 import android.graphics.Color;
 import android.provider.Settings;
-import android.view.View;
 import android.content.Intent;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.res.Configuration;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
-public class MainActivity extends Activity
-{
+public class MainActivity extends Activity {
     public final static String IP_ERROR_MESSAGE = "com.northstar.minimap.MESSAGE";
 
+    private String[] drawerListViewItems;
+    private DrawerLayout drawerLayout;
+    private ListView drawerListView;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerListViewItems = getResources().getStringArray(R.array.items);
+        drawerListView = (ListView) findViewById(R.id.left_drawer);
+        drawerListView.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_listview_item, drawerListViewItems));
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                drawerLayout,         /* DrawerLayout object */
+                R.drawable.ic_drawer,  /* nav drawer icon to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description */
+                R.string.drawer_close  /* "close drawer" description */
+        );
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        drawerListView.setOnItemClickListener(new DrawerItemClickListener());
     }
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
+    public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
-    public void submitIP(View view)
-    {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        actionBarDrawerToggle.syncState();
+    }
+
+    public void submitIP(View view) {
         Intent mapIntent = new Intent(this, MapActivity.class);
         EditText ipTextbox = (EditText) findViewById(R.id.server_ip);
         String serverIP = ipTextbox.getText().toString();
         String ipErrorMessage = "no error!";
 
-        try
-        {
+        try {
             Globals state = (Globals)getApplicationContext();
             state.comm.setServerIP(InetAddress.getByName(serverIP));
             mapIntent.putExtra(IP_ERROR_MESSAGE, ipErrorMessage);
             startActivity(mapIntent);
-        }
-        catch(Exception e)
-        {
+        } catch(Exception e) {
             // Incorrect IP address format
             ipErrorMessage = e.getMessage();
 
@@ -57,8 +104,14 @@ public class MainActivity extends Activity
             errorText.setTextColor(Color.RED);
             ipTextbox.setText("");
         }
-
-
     }
-    
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            Toast.makeText(MainActivity.this, ((TextView)view).getText(), Toast.LENGTH_LONG).show();
+            drawerLayout.closeDrawer(drawerListView);
+
+        }
+    }
 }
