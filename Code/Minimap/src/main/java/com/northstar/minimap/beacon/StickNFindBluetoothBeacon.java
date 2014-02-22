70ee84824c1ca437072ed58e1c669559298363d9
Chris Carey <cpc2273@rit.edu>
2014-02-22 14:14:40 -0500

    Apply smoothing.

--- Code/Minimap/src/main/java/com/northstar/minimap/beacon/StickNFindBluetoothBeacon.java
+++ Code/Minimap/src/main/java/com/northstar/minimap/beacon/StickNFindBluetoothBeacon.java
@@ -7,14 +7,20 @@ import com.northstar.minimap.Position;
 import android.bluetooth.BluetoothDevice;
 
 import java.text.DecimalFormat;
+import java.util.LinkedList;
 
 public class StickNFindBluetoothBeacon extends BluetoothBeacon {
 
     public static final double PROPAGATION_CONSTANT = 1.1;
     public static final int RSSI_AT_ONE_METER = -75;
+    public static final int SMOOTHING_RANGE = 5;
+
+    private LinkedList<Double> previousDistances;
 
     public StickNFindBluetoothBeacon(BluetoothDevice device, String beaconID, Position location) {
         super(device, beaconID, location);
+
+        previousDistances = new LinkedList<Double>();
     }
 
     /**
@@ -32,11 +38,27 @@ public class StickNFindBluetoothBeacon extends BluetoothBeacon {
      */
     @Override
     public Double computeDistance() {
-        return Math.pow(10, (RSSI_AT_ONE_METER - signalStrength) / (10.0 * PROPAGATION_CONSTANT));
-    }
+        double distance = rssiToDistance(signalStrength);
+        double smoothedDistance = 0.0;
 
+        if (previousDistances.size() == SMOOTHING_RANGE) {
+            previousDistances.removeFirst();
+        }
+
+        previousDistances.addLast(distance);
+
+        for (double previousDistance: previousDistances) {
+            smoothedDistance += previousDistance;
+        }
+
+        return (smoothedDistance / previousDistances.size());
+    }
 
     public String getFormattedDistance() {
         return (new DecimalFormat("#.##").format(computeDistance()) + " m");
+    }
+
+    private double rssiToDistance(int rssi) {
+        return Math.pow(10, (RSSI_AT_ONE_METER - rssi) / (10.0 * PROPAGATION_CONSTANT));
     }
 }
