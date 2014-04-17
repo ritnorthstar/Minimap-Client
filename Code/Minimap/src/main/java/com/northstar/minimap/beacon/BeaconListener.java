992cf149698d2b576c5f98b1976aef99f703a97e
Chris Carey <cpc2273@rit.edu>
2014-04-17 18:05:25 -0400

    Cleanup.

--- Code/Minimap/src/main/java/com/northstar/minimap/beacon/BeaconListener.java
+++ Code/Minimap/src/main/java/com/northstar/minimap/beacon/BeaconListener.java
@@ -1,8 +1,5 @@
 package com.northstar.minimap.beacon;
 
-/**
- * Created by Chris on 4/11/14.
- */
 public interface BeaconListener {
     public void onBeaconDistanceChanged(IBeacon beacon, double distance);
 }
